512b93729c9c4913e071cf1268df83e1634c1a6e
antonBlagodarnyy <antonbgi98@gmail.com>
2025-04-04 17:08:26 +0200

    products-appointments table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
     //
+    public function appointments(): BelongsToMany
+    {
+        return $this->belongsToMany(Appointment::class);
+    }
 }
