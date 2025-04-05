fda5b5d24bcda53abd88350d0e2cd9d64387f906
antonBlagodarnyy <antonbgi98@gmail.com>
2025-04-05 13:04:00 +0200

    created login form

--- resources/views/livewire/signup.blade.php
+++ resources/views/livewire/signup.blade.php
@@ -1,3 +1,11 @@
-<div>
-   <h1>Signup works</h1>
-</div>
+<form >
+    <label for="name">Nombre</label>
+    <input name="name" id="name" type="text" />
+    <label for="email">Email</label>
+    <input name="name" id="name" type="text" />
+    <label for="password">Contraseña</label>
+    <input name="password" id="password" type="text" />
+    <label for="cPassword">Confirmar contraseña</label>
+    <input name="cPassword" id="cPassword" type="text" />
+    <button type="submit">Enviar</button>
+</form>
