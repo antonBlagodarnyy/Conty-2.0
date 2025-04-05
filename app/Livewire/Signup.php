79e15974e249c4507cdbc93ca995a8a302638a65
antonBlagodarnyy <antonbgi98@gmail.com>
2025-04-05 17:53:47 +0200

    livewire components act as controllers

--- app/Livewire/Signup.php
+++ app/Livewire/Signup.php
@@ -4,8 +4,12 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+
 class Signup extends Component
 {
+    public function save()
+    {
+    }
     public function render()
     {
         return view('livewire.signup');
--- resources/views/livewire/signup.blade.php
+++ resources/views/livewire/signup.blade.php
@@ -1,11 +1,11 @@
-<form >
+<form wire:submit="save">
     <label for="name">Nombre</label>
     <input name="name" id="name" type="text" />
     <label for="email">Email</label>
     <input name="name" id="name" type="text" />
     <label for="password">Contraseña</label>
     <input name="password" id="password" type="text" />
-    <label for="cPassword">Confirmar contraseña</label>
-    <input name="cPassword" id="cPassword" type="text" />
+    <label for="password_confirmation">Confirmar contraseña</label>
+    <input name="password_confirmation" id="password_confirmation" type="text" />
     <button type="submit">Enviar</button>
 </form>
