1724fda7ec573939c356e5fd33a8d91cf4843817
antonBlagodarnyy <antonbgi98@gmail.com>
2025-04-09 12:41:37 +0200

    flux works

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,10 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        @vite('resources/css/app.css')
+        @fluxAppearance
         <title>{{ $title ?? 'Page Title' }}</title>
     </head>
     <body>
         {{ $slot }}
+        @fluxScripts
     </body>
 </html>
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,3 +1,8 @@
 <div>
-    <livewire:navbar/>
+<flux:navbar>
+    <flux:navbar.item href="#">Home</flux:navbar.item>
+    <flux:navbar.item href="#">Features</flux:navbar.item>
+    <flux:navbar.item href="#">Pricing</flux:navbar.item>
+    <flux:navbar.item href="#">About</flux:navbar.item>
+</flux:navbar>
 </div>
