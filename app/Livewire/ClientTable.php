e5efa97cc73bfa5522b82a44015f6e1946b57994
antonBlagodarnyy <antonbgi98@gmail.com>
2025-04-13 12:58:32 +0200

    created table and started deleting

--- app/Livewire/ClientTable.php
+++ app/Livewire/ClientTable.php
@@ -2,18 +2,33 @@
 
 namespace App\Livewire;
 
+use Barryvdh\Debugbar;
 use App\Models\Client;
+use Illuminate\Database\Eloquent\Casts\Json;
+use Illuminate\Support\Enumerable;
 use RamonRietdijk\LivewireTables\Livewire\LivewireTable;
 use RamonRietdijk\LivewireTables\Columns\Column;
+use RamonRietdijk\LivewireTables\Actions\Action;
 
 class ClientTable extends LivewireTable
 {
     protected string $model = Client::class;
 
-    protected function columns(): array{
+
+    protected function columns(): array
+    {
         return [
-            Column::make(__('Nombre'), 'name'),
-            Column::make(__('Telefono'),'phone'),
+            Column::make(__('Nombre'), 'name')
+                ->searchable()
+                ->sortable(),
+            Column::make(__('Telefono'), 'phone'),
+        ];
+    }
+
+    protected function actions(): array
+    {
+        return [
+            Action::make(__('Eliminar cliente'), 'confirm("Seguro que desea eliminar a los clientes seleccionados?",$wire.$parent.deleteClient($wire.selected))'),
         ];
     }
 }
--- app/Livewire/Clients.php
+++ app/Livewire/Clients.php
@@ -4,9 +4,12 @@ namespace App\Livewire;
 
 
 use Livewire\Component;
-
+use App\Models\Client;
 
 class Clients extends Component
 {
 
+    public function deleteClient($id){
+        Client::where('id',$id)->delete();
+    }
 }
