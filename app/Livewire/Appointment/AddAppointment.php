91940cea4f67aa77eb613a2290a01bd2ea239e17
antonBlagodarnyy <antonbgi98@gmail.com>
2025-04-21 10:45:15 +0200

    selected products must have quantity

--- app/Livewire/Appointment/AddAppointment.php
+++ app/Livewire/Appointment/AddAppointment.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Appointment;
 
 use App\Models\Appointment;
+use App\Rules\selectedHaveQuantityRule;
 use Livewire\Component;
 
 
@@ -10,18 +11,20 @@ class AddAppointment extends Component
 {
     public $clientSelection, $products, $date, $job;
 
-    public function save(){
-        error_log(json_encode($this->products));
-
+    public function save()
+    {
+        $this->validate();
     }
     protected function rules()
     {
         return [
             'date' => 'required',
             'job' => 'required',
-            'clientSelection' => 'required',
+            'clientSelection' => 'array|size:1',
+            'products' => new selectedHaveQuantityRule(),
         ];
     }
+
     public function render()
     {
         return view('livewire.appointment.add-appointment');
---
+++ app/Rules/selectedHaveQuantityRule.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class selectedHaveQuantityRule implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        //
+        foreach ($value['selected'] as $selected) {
+            if (!isset($value['quantity'][intval($selected)]) || $value['quantity'][intval($selected)] == "") {
+                $fail('All selected products must have a quantity.');
+            }
+        }
+      
+    }
+}
