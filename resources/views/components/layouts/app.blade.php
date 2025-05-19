8a490e5bc6eedf82ac683a55376b652f44174c2c
antonBlagodarnyy <antonbgi98@gmail.com>
2025-05-19 18:22:48 +0200

    added js to vite build

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @vite('resources/css/app.css')
-        @fluxAppearance
-   
-        <title>{{ $title ?? 'Page Title' }}</title>
-    </head>
-    <body >
-        {{ $slot }}
-        @fluxScripts
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/js/app.js', 'resources/css/app.css')
+    @fluxAppearance
+
+    <title>{{ $title ?? 'Page Title' }}</title>
+</head>
+
+<body>
+    {{ $slot }}
+    @fluxScripts
+</body>
+
 </html>
