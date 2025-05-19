cfbcc97d170f9206bc40efdc036b46649b0a957b
antonBlagodarnyy <antonbgi98@gmail.com>
2025-05-19 19:10:54 +0200

    deleted middleware that does not work and changed vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: 'https://conty-20-production.up.railway.app/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
